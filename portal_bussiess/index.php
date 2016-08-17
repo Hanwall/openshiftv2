eb3fd78e61d52765ea74cac8ef616284721aea20
username <username@hostname.com>
2016-08-17 22:37:00 +0800

    1111111111

--- portal_bussiess/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-chdir('../');
-define('SUB_DIR', '/portal_bussiess/');
-$_GET['mod'] = 'list';
-$_GET['catid'] = '7';
-require_once './portal.php';
-?>
--- portal_ebank/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-chdir('../');
-define('SUB_DIR', '/portal_ebank/');
-$_GET['mod'] = 'list';
-$_GET['catid'] = '8';
-require_once './portal.php';
-?>
