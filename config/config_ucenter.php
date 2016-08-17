2c979c9ace434464a3529e960cdb05dac8d3aabc
username <username@hostname.com>
2016-08-17 18:59:48 +0800

    new configure from hknode

--- config/config_ucenter.php
+++ config/config_ucenter.php
@@ -13,8 +13,7 @@ define('UC_DBCONNECT', 0);
 
 define('UC_CHARSET', 'utf-8');
 define('UC_KEY', 'q1Ec34G7s9PaybLbf6n4c5O9gc3fzbS6u4j13e6b11m5Wfi8214cE9h6dfzb0199');
-define('UC_API', 'http://huizhouhs.com/bbs/uc_server');
+define('UC_API', 'http://xuibi.huizhouhs.com/uc_server');
 define('UC_APPID', '1');
 define('UC_IP', '');
 define('UC_PPP', 20);
-?>
