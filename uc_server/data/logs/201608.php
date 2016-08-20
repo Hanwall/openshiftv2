2c8a4e080c03313b4518bee1486c403c9348f76b
username <username@hostname.com>
2016-08-20 18:13:10 +0800

    backup first time for reset status

--- uc_server/data/logs/201608.php
+++ uc_server/data/logs/201608.php
@@ -1,1 +1,4 @@
 <?PHP exit;?>	UCenterAdministrator	127.0.0.1	1471453512	login	succeed
+<?PHP exit;?>	UCenterAdministrator	115.199.55.147	1471516871	login	succeed
+<?PHP exit;?>	UCenterAdministrator	115.199.55.147	1471516933	app_edit	appid=1
+<?PHP exit;?>	UCenterAdministrator	220.191.57.190	1471683738	login	succeed
